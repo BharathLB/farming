67c02b5b88ec3e7c6603589e6c40d8cd051d1c72
First Last <bharath@active.agency>
2023-07-29 12:11:20 +0530

    component

---
+++ src/Components/Home.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+function demo(){
+    <style>
+        {/* .phone{
+            color:white;
+            font-size:15px;
+        }
+        .envelope{
+            font-size:15px;
+            color:white;
+            margin-left:10px;
+        } */}
+    </style>
+    return(
+        <>
+        <div class="he">
+             <div>
+                <i class="fa fa-phone">   +91 9856748987</i>
+                 <i class="fa fa-envelope">   agrimachin.gmail.com</i>
+            </div>
+            <div class="my">
+                <button href="#">Shop</button><span class="vl"></span>
+                <button href="#">Cart</button><span class="vl"></span>
+                <button href="#">Account</button>
+            </div>
+        </div>
+        <div class="navsec">
+            <div><h1>FARMING</h1></div>
+            <div class="navba"> 
+                <button value="./apex/Home" styleclass="as">Home</button>
+                <button value="./apex/About" styleclass="as">About</button>
+                <button value="./apex/About" styleclass="as">Pages</button>
+                <button value="./apex/About" styleclass="as">Blogs</button>
+                <button value="./apex/Contactuss" styleclass="as">Contact</button>
+            </div>
+        </div>
+
+        </>
+    )
+}
+export default demo;
